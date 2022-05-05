a9d815acf3458bb3209171df93f0293ad6c0a72f
Julian <jzethar@gmail.com>
2022-05-06 02:24:16 +0300

    fixes and add tests

--- lesson8/src/main/kotlin/ru/tinkoff/fintech/lesson8/ThreadPool.kt
+++ lesson8/src/main/kotlin/ru/tinkoff/fintech/lesson8/ThreadPool.kt
@@ -6,11 +6,14 @@ import java.util.concurrent.LinkedBlockingQueue
 
 class ThreadPool(private val poolSize: Int = 5) : Executor {
 
-    private val workers: Array<WorkerThread?> = arrayOfNulls(poolSize)
+    private lateinit var workers: Array<WorkerThread?>
     private val queue: LinkedBlockingQueue<Runnable> = LinkedBlockingQueue()
+    private var pSize :  Int = 0
 
     init {
-        for (i in 0 until if (poolSize <= 5) poolSize else 5) {
+        pSize = if (poolSize <= 5) poolSize else 5
+        workers = arrayOfNulls(pSize)
+        for (i in 0 until pSize) {
             workers[i] = WorkerThread(queue)
             workers[i]!!.start()
         }
@@ -23,9 +26,11 @@ class ThreadPool(private val poolSize: Int = 5) : Executor {
         }
     }
 
+    fun getSizeOfQueue() = workers.size
+
     fun shutdown() {
-        println("Shutting down")
-        for (i in 0 until poolSize) {
+        //println("Shutting down")
+        for (i in 0 until pSize) {
             workers[i]?.workUntilAlive = false
         }
         synchronized(queue as Object) {
---
+++ lesson8/src/test/kotlin/ru/tinkoff/fintech/tests/ThreadTest.kt
@@ -0,0 +1,50 @@
+package ru.tinkoff.fintech.tests
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import ru.tinkoff.fintech.lesson8.Task
+import ru.tinkoff.fintech.lesson8.ThreadPool
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+
+class ThreadTest {
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun `test counter`() {
+        val numberOfThreads = 5
+        val service = ThreadPool(5)
+        val latch = CountDownLatch(numberOfThreads)
+        var counter = 0
+        for (i in 0 until numberOfThreads) {
+            service.execute {
+                counter += 1
+                latch.countDown()
+            }
+        }
+        latch.await()
+        service.shutdown()
+        assertEquals(numberOfThreads, counter)
+    }
+
+    @Test
+    fun `test pool size`() {
+        val service = ThreadPool(10)
+        service.shutdown()
+
+        assertEquals(5, service.getSizeOfQueue())
+    }
+
+    @Test
+    fun `test shutdown service`() {
+        val service = ThreadPool(10)
+        var counter = 0
+        service.shutdown()
+        service.execute {
+            counter += 1
+        }
+
+        assertEquals(0, counter)
+    }
+}
