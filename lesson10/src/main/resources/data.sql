95cbb0437937b7d07f8274abbe62c98e5dc259f7
Julian Volianskiy <49489282+juy13@users.noreply.github.com>
2022-05-22 20:40:25 +0300

    Lesson9 (#9)
    * new lesson 9
    
    * fixes
    
    * bug with tests
    
    * add resources
    
    * fixes + tests
    
    Co-authored-by: Julian <jzethar@gmail.com>

---
+++ lesson10/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO EVENTS (id, type, body, status) VALUES(0, 'SMS', 'qwerty', 'NEW');
+INSERT INTO EVENTS (id, type, body, status) VALUES(1, 'EMAIL', 'asdfg', 'NEW');
+INSERT INTO EVENTS (id, type, body, status) VALUES(2, 'EMAIL', 'zxcvbn', 'NEW');
+INSERT INTO EVENTS (id, type, body, status) VALUES(3, 'PUSH', 'qazwsxedc', 'NEW');
---
+++ lesson9/src/main/kotlin/ru/tinkoff/fintech/lesson9/Lesson6Application.kt
@@ -0,0 +1,11 @@
+package ru.tinkoff.fintech.lesson9
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lesson9Application
+
+fun main() {
+	runApplication<Lesson9Application>()
+}
